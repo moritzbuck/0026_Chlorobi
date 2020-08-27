323f63c8c5275c487d38e4ba3860b9fb05589cde
moritzbuck <moritz.buck@slu.se>
2020-08-27 09:52:20 +0200

    cleanup

--- oups.py
+++
@@ -1,5 +0,0 @@
-trouts = {"IHWH" : "IMG:3300020735",
-"IHXT" : "IMG:3300020721",
-"IHUI" : "IMG:3300020685"}
-
-all_samples.update( {s : {'dataset' : 'troutbog', 'accession' : trouts[s]} for s in samples3})
